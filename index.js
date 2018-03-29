4b486144a38f6a3098c0a77203ab98ad5ec935b5
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function Scooter(year, color, model) {
-  [['year', year], ['model', model], ['color', color]].forEach((key, value) => {
-    this[key] = value
-  })
+  this.year = year
+  this.color = color
+  this.model = model
 }
